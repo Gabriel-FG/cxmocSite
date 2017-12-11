b45d6f9e5d026683effdf6314fe2c322c9077cd7
Gabriel-FG <gabrielfigueiredo27@hotmail.com>
2017-12-11 12:37:51 -0200

    add

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends MY_Controller{
 	function __construct(){
+		echo "aqui";
 		parent:: __construct();
 		$this->load->model('noticias_model');
 		$this->load->model('imagens_model');
